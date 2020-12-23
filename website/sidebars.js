609970bf7a7bf2eedbf7896f0b296164fffdd326
Greeshma Merin varghese <greeshma@dexlock.com>
2020-12-23 13:40:08 +0530

    Modified the side bar

--- website/sidebars.js
+++ website/sidebars.js
@@ -14,7 +14,7 @@ module.exports = {
       "radiology",
       "chiefComplaints",
       "ManageConsentForm",
-      "generalSettings",
+      "hl7Listeners",
       "emailTemplate",
       "generalSettings",
     ],
